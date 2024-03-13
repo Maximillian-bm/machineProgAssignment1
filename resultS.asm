c7f6474e6d34c3b9c8125d7c1ff063af12659e06
PhillipNguyenCong <144035124+PhillipNguyenCong@users.noreply.github.com>
2024-03-13 19:58:17 +0100

    DoneAss4WithPUML

--- resultS.asm
+++ resultS.asm
@@ -1,13 +1,11 @@
 .orig x3000
-resultS ST R1, save1
-        AND R0,R0, #0                       ;sets R0 to 0
+resultS AND R0,R0, #0                       ;sets R0 to 0
         
 result  ADD R0,R0, #1                       ;sets R0 to the given nr
         BRz resultZ
         BRnp resultNP 
-        
-done    LD R1, save1
-        RET
+        JSR     EOP                         ;this stops the program after the outprint
+
 
 outputIs  .stringz "The number is prime"    ;this is the output string that resultZ is holding
 resultZ LEA R0, outputIs                    ;sets the output
@@ -19,7 +17,5 @@ resultNP LEA R0, outputNot                  ;sets the out output
         PUTS                                ;prints it in console 
         JSR     EOP                         ;this stops the program after the outprint
         
-save1   .BLKW 1
-
 EOP 
 .end                                    ;this labels .end with EOP(endOfProgram)
