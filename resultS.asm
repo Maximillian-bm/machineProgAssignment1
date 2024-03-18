0e3f99930a0ec0a43da318c4115975009125ca96
PhillipNguyenCong <144035124+PhillipNguyenCong@users.noreply.github.com>
2024-03-18 13:19:48 +0100

    prime

--- resultS.asm
+++ resultS.asm
@@ -7,7 +7,7 @@ result  ADD R0,R0, #1                       ;sets R0 to the given nr
         JSR     EOP                         ;this stops the program after the outprint
 
 
-outputIs  .stringz "The number is prime"    ;this is the output string that resultZ is holding
+outputIs  .stringz "The number is prime"    ;this is the output string that resultP is holding
 resultP LEA R0, outputIs                    ;sets the output
         PUTS                                ;prints its in console
         JSR     EOP                         ;this stops the program after the outprint
