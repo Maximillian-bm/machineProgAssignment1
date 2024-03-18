e821296d796e0702eb3b0f3c90b55ff62e7985b5
Max <maxwork.bm@gmail.com>
2024-03-18 14:19:34 +0100

    comments

--- Assignment5.asm
+++ Assignment5.asm
@@ -9,70 +9,70 @@ JSR     resultS
 JSR     SOP
 
 ; ***** Read a 2 digit decimal number from the console. *****
-readS   ST R1, save1
+readS   ST R1, save1 ; Save the registeres values in memory
         ST R2, save2
         ST R3, save3
-        AND R3, R3, #0
+        AND R3, R3, #0 ; Set R3 to -48
         ADD R3, R3, #-16
         ADD R3, R3, #-16
         ADD R3, R3, #-16
-        LEA R0, output
-        PUTS
-read    GETC
-        OUT
-        ADD R0, R0, R3
-        AND R2, R2, #0
-        ADD R2, R2, #10
-        AND R1, R1, #0
+        LEA R0, output ; Set R0 to be a pointer for the output string
+        PUTS ; Print the string
+read    GETC ; Read the next input
+        OUT ; Echo the input back out
+        ADD R0, R0, R3 ; Subtract 48 from the input to get the value
+        AND R2, R2, #0 ; Set R2 to 10
+        ADD R2, R2, #10 
+        AND R1, R1, #0 ; Set R1 to 0
 loop    ADD R1, R1, R0
         ADD R2, R2, #-1
-        BRp loop
-        GETC
-        OUT
-        ADD R0, R0, R3
-        ADD R0, R0, R1
-output  .stringz "\nInput a 2 digit decimal number: "
-        LD R1, save1
+        BRp loop ; Add the input value to R1 10 times
+        GETC ; Get the next input
+        OUT ; Echo the input back out
+        ADD R0, R0, R3 ; Subtract 48 from the input to get the value
+        ADD R0, R0, R1 ; Add the input times 10 to the new input
+output  .stringz "Input a 2 digit decimal number:" 
+        LD R1, save1 ; Restore the registeres values
         LD R2, save2
         LD R3, save3
-        RET
+        RET ; Return from subroutine
 save1   .BLKW 1
 save2   .BLKW 1
 save3   .BLKW 1
 
 ; ***** Check if number is prime. *****
-isPrime ST R1, save4
-        ST R2, save5
-        ST R3, save6
-        ST R4, save7
-        ST R5, save8
-        ADD R1, R0, #0
-        AND R5, R5, #0
-loop1   ADD R5, R5, #1
-        ADD R1, R1, #-2
-        BRzp loop1
-        ADD R1, R5, #0
-loop2   ADD R1, R1, #-1
-        ADD R3, R1, #-1
-        BRnz is
-        ADD R2, R0, #0
-        ADD R4, R1, #0
+isPrime ST R1, save1 ; Save the value of registers to memory
+        ST R2, save2
+        ST R3, save3
+        ST R4, save4
+        ST R5, save5
+        ADD R1, R0, #0 ; Set R1 equal to the agument
+        AND R5, R5, #0 ; Set R5 to 0
+loop1   ADD R5, R5, #1 ; Add 1 to R5
+        ADD R1, R1, #-2 ; Subtract 2 from R1
+        BRzp loop1 ; Repeat loop if R1 isnt negativ
+        ADD R1, R5, #0 ; Set R1 equal to R5
+loop2   ADD R1, R1, #-1 ; Subtract 1 from R1
+        ADD R3, R1, #-1 ; Set R3 equal to R1 minus 1
+        BRnz is ; If R3 is less than 1 jump to 'is'
+        ADD R2, R0, #0 ; Set R2 equal to the agument
+        ADD R4, R1, #0 ; Set R4 equal to R1
         NOT R4, R4
         ADD R4, R4, #1
-        ADD R2, R2, R4
-loop3   ADD R2, R2, R4
-        BRp loop3
-        BRn loop2
-        AND R0, R0, #0
-        BR done
+        ADD R2, R2, R4 ; Subtract R4 from R2 outside loop3
+loop3   ADD R2, R2, R4 ; Subtract R4 from R2 inside loop3
+        BRp loop3 ; Repeat loop3 if R2 is posetiv
+        BRn loop2 ; Repeat loop2 if R2 is negative
+        AND R0, R0, #0 ; Set R0 to 0
+        BR done ; Jump to 'done'
 is      AND R0, R0, #0
-        ADD R0, R0, #1
-done    LD R1, save4
-        LD R2, save5
-        LD R3, save6
-        LD R4, save7
-        LD R5, save8
-        RET
+        ADD R0, R0, #1 ; set R0 to 1
+done    LD R1, save1 ; Restore the values of the registeres
+        LD R2, save2
+        LD R3, save3
+        LD R4, save4
+        LD R5, save5
+        RET ; Return from subroutine
 save4   .BLKW 1
 save5   .BLKW 1
 save6   .BLKW 1
